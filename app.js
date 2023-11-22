19281727d138d1748c19342e57a400209669cd32
Leon Schwedler <leon@EO-LA-021>
2023-11-22 11:25:52 +0100

    add a rain request

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 const textField = document.getElementById("textOutput");
 const button = document.getElementById("submit");
-const form = document.getElementById("input");
 const latitude = document.getElementById("latitude");
 const longitude = document.getElementById("longitude");
-let dataJson = "";
-
-
+let percipitation = "";
 
 async function getTemperatureForLatLong(){
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude.value}&longitude=${longitude.value}&current=temperature_2m`
-}
-
-
-
-async function getDataJson() {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude.value}&longitude=${longitude.value}&current=temperature_2m&daily=weather_code`
     const dataPromise = await fetch(url);
-    dataJson = await dataPromise.json();
-    console.log(dataJson);
-}
-getDataJson();
+    let dataJson = await dataPromise.json();
+    console.log("dataJson", dataJson);
+    // Regenabfrage:
+    if(dataJson.daily.weather_code == 60 | dataJson.daily.weather_code == 61) {
+        percipitation = "Heute empfehle ich mindestens eine Regenjacke!";
+    } else if(dataJson.daily.weather_code == 62 | dataJson.daily.weather_code == 63) {
+        percipitation = "Heute empfehle ich dir eine Regenjacke UND einen Schirm!";
+    } else if(dataJson.daily.weather_code == 64 | dataJson.daily.weather_code == 65) {
+        percipitation = "Heute empfehle ich dir zu hause zu bleiben oder ein Boot zu nehmen!";
+    } else {
+        percipitation = "Heute bleibt es trocken!";
+    }
 
-
-button.addEventListener("click", getTemperature);
-function getTemperature() {
-    const tempValue = dataJson.current.temperature_2m + " " + dataJson.current_units.temperature_2m;
+    const tempValue = "Es sind aktuell " + dataJson.current.temperature_2m + dataJson.current_units.temperature_2m + "!" + "\n" + percipitation;
     textField.value = tempValue;
 }
+button.addEventListener("click", getTemperatureForLatLong);
+
+
 
 
